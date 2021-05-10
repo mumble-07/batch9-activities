8fc50910859c976f88504ae129ddc2bbe0909bad
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-05-10 22:37:04 +0800

    coding-challenges: initial commit

---
+++ js-challenges-and-projects-v.1.2/intro-to-loops/color-my-chessboard!/app.js
@@ -0,0 +1,5 @@
+const squares = Array.from(document.querySelectorAll('.grid div'))
+//Nodelist of all the div squares on our board. Think of it as an array. 
+
+//Your goal is to add a chessboard color pattern to this blank board using loops and Arrays.
+//write code here
---
+++ js-challenges-and-projects-v.1.2/js-overview/product-landing-page/index.js
@@ -0,0 +1,13 @@
+const el1 = document.getElementById ("container2a");
+const el2 = document.getElementById ("nav-links");
+
+el1.innerHTML = 'Testing for JS'
+el1.style.backgroundColor = 'blue'
+
+for (let cnt=0; cnt < (elements.length-1)); cnt++)
+{
+    console.log (' ')
+    el2[cnt].innerHTML = 'Testing for JSSSS'
+    el2[cnt].style.backgroundColor = 'red'
+}
+
