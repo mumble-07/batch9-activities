60544e21d960a2eab581ce148c71bd08eb7e70a6
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-05-14 19:29:58 +0800

    coding-challenges: modified whats in the fridge challenge with the help of classmamtes

--- js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/whats-in-the-fridge/app.js
+++ js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/whats-in-the-fridge/app.js
@@ -10,37 +10,41 @@ const fridge = [' Koko Krunch', ' OJ ', ' Cowhead', ' Lays ', ' Ovalteenies']
 
 //Challenge 2: You have bought some chicharon.
 //Please remove it from the buyList and add it to the items in fridge.
-let boughtItem = buyList.shift()
-    fridge.push(boughtItem)
 
 //Challenge 3: Write a function that will remove an item from the fridge,
 //and put it in the buyList, on the press of the moveUp button.
 
-function moveUp(){
-    //your code
-    let eatItem = fridge.pop()
-    buyList.push(eatItem)
-    showList ()
-
-}
-
-upButton.addEventListener('click', moveUp)
-
 //Challenge 4: Write a function that will remove the last item in the buyList, 
 //and put it in the fridge.
 
-function moveDown(){
-    //your code
-    let boughtItemLast = buyList.pop()
-    fridge.push(boughtItemLast)
-    showList ()
+//TESTING WITH STOP//
 
-}
-function showList() {
+let boughtItem = buyList.shift()
+    fridge.push(boughtItem)
+
+    function showList() {
         buyListDisplay.innerHTML = buyList
         fridgeListDisplay.innerHTML = fridge
-} 
+    } 
+    showList ()
 
+    function moveUp(){
+        if (fridge.length > 0) {
+            let eatItem = fridge.pop()
+            buyList.push(eatItem)
+            showList ()
+        }
+    }
+
+upButton.addEventListener('click', moveUp)
+
+
+    function moveDown(){
+        if (buyList.length > 0) {
+            let boughtItemLast = buyList.pop()
+            fridge.push(boughtItemLast)
+            showList ()
+        }
+    }
 
 downButton.addEventListener('click', moveDown)
-    showList ()
