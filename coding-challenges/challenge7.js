2a0b86f90a4acdac56851910e678a035493491bb
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-05-14 14:54:27 +0800

    coding-challenges: adding coding challenges from course review

---
+++ coding-challenges/challenge7.js
@@ -0,0 +1,14 @@
+//Create an if-else statement that logs if the object property is true or false
+
+var user = {
+    name: "Bruce",
+    email: null,
+    friends: ["Aflred", "Robin"],
+    address: {
+        street: "123 wayne manor",
+        city: "Gotham"
+    },
+    id: 0,
+    nickname: undefined
+}
+
