dced2b536cb5a56dd95e63beedfff6d37a1cf858
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-05-14 20:59:42 +0800

    coding-challenges: adding name-that-superpower challenge and eventListener challenge

--- js-challenges-and-projects-v.1.2/intro-to-dom/name-that-superpower/app.js
+++ js-challenges-and-projects-v.1.2/intro-to-dom/name-that-superpower/app.js
@@ -5,3 +5,12 @@ var justiceLeague = [
     {name: "The Flash", superpower: "Super speed"}, 
     {name: "Green Lantern", superpower: "Super ring"}
 ]
+
+var leagueList = document.getElementById("league");
+
+for (i=0; i < justiceLeague.length; i++) {
+
+    let newLeague = document.createElement("li");
+    newLeague.textContent = justiceLeague[i].name + " " + justiceLeague[i].superpower;
+    leagueList.append(newLeague);
+}
