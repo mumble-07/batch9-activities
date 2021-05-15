9b630a298bfd518227d25346d028549adaa311bf
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-05-16 00:01:38 +0800

    momentum-app: initial commit

---
+++ 6-momentum-app/js/clock.js
@@ -0,0 +1,18 @@
+const clockJs = document.querySelector ("#clock")
+const clockText = clockJs.querySelector ("h1")
+
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes =date.getMinutes();
+    const seconds = date.getSeconds();
+    clockText.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    }
+
+
+    function showTime() {
+        getTime();
+        setInterval (getTime, 1000);
+    }
+    
+    showTime();
