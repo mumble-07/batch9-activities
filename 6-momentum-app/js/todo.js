da562d66c988c5e4d9d673fc96c463169955609b
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-05-18 19:16:15 +0800

    momentum-app: adding todoList js

--- 6-momentum-app/js/todo.js
+++ 6-momentum-app/js/todo.js
@@ -1,11 +1,51 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoSection');
+let inputField = document.getElementById('inputTodojs');
+
+addToDoButton.addEventListener('click', function(event){
+  event.preventDefault();
+
+    var todoList = document.createElement('p');
+    todoList.classList.add('todoList-style');
+
+    todoList.innerText = inputField.value;
+    toDoContainer.appendChild(todoList);
+    inputField.value = "";
+
+    todoList.addEventListener('click', function(){
+    todoList.style.textDecoration = "line-through";
+    todoList.style.color = "red";
+    })
+
+    todoList.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(todoList);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* 
 const listTodo = document.querySelector(".js-listTodo"); */
 
-let todoItem = [];
+/* let todoItem = [];
 
 function renderTodo (todo) {
   const list = document.querySelector('js-toDoList');
-  
+  const item = document.querySelector(`[data-key='${todo.id}']`);
   const isChecked = todo.checked ? 'done': '';
   const node = document.createElement("li");
   node.setAttribute('class', `todo-item ${isChecked}`);
@@ -18,20 +58,30 @@ function renderTodo (todo) {
     <svg><use href="#delete-icon"></use></svg>
     </button>
   `;
+
+  if (item) {
+    list.replaceChild(node, item);
+  }
+  else {
   list.append(node);
+  }
 }
-
-
 
 function addTodo(text) {
   const todo = {
     text,
     checked: false,
     id: Date.now(),
-  };
-  todoItem.push(todo);
+  }; 
+  todoItem.push(todo);*/
   /* console.log(todoItem); */
-  renderTodo(todo);
+/*   renderTodo(todo);
+}
+
+function toggleDone(key) {
+   const index = todoItem.findIndex(item => item.id === Number(key));
+  todoItem[index].checked = !todoItem[index].checked;
+  renderTodo(todoItem[index]);
 }
 
 const formTodo = document.querySelector(".todojs");
@@ -46,4 +96,11 @@ formTodo.addEventListener('submit', event =>{
   }
 });
 
+const list = document.querySelector(".js-toDoList");
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+}); */
 
