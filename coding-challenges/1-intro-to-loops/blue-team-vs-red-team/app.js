06f0289c786e8d62a379f786bd2a40aada213567
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-05-26 21:03:15 +0800

    main: arranging folders

--- coding-challenges/1-intro-to-loops/blue-team-vs-red-team/app.js
+++
@@ -1,8 +0,0 @@
-const people = 58;
-
-for (let i=1; i <= people; i++) {
-    if (i % 2 === 0) {
-        console.log('blue team')
-    }
-    else {console.log ('red team')}
-}
--- coding-challenges/4-intro-to-js/challenge8.js
+++
@@ -1,5 +0,0 @@
-/* homework: 
-After going through the lesson, navigate to your forked Precourse repo > Lesson 8: Introduction to JavaScript (Part 5), 
-and accomplish the homework file.
-Don't forget to push your updates in your repo! Good luck 😄 */
-
